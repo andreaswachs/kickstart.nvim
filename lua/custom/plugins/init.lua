0dd3c162ae834b3222b49d6bf6017330e57a6036
Andreas Wachs <andreas.wachs@maersk.com>
2023-09-01 10:01:19 +0200

    Format

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -75,25 +75,25 @@ local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').goimport()
+    require('go.format').goimport()
   end,
   group = format_sync_grp,
 })
 
 return {
   {
-  "ray-x/go.nvim",
-  dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("go").setup()
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
   {
     's1n7ax/nvim-window-picker',
@@ -101,7 +101,7 @@ return {
     event = 'VeryLazy',
     version = '2.*',
     config = function()
-        require'window-picker'.setup()
+      require'window-picker'.setup()
     end,
   },
   {

115c35c7153657fbd007aaca3adffb22c9431673
Andreas Wachs <andreas.wachs@zeronorth.com>
2024-02-15 09:13:03 +0100

    Add autopairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -122,6 +122,21 @@ require("tokyonight").setup({
 
 return {
     {
+        "windwp/nvim-autopairs",
+        -- Optional dependency
+        dependencies = { 'hrsh7th/nvim-cmp' },
+        config = function()
+            require("nvim-autopairs").setup {}
+            -- If you want to automatically add `(` after selecting a function or method
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on(
+                'confirm_done',
+                cmp_autopairs.on_confirm_done()
+            )
+        end,
+    },
+    {
         "https://github.com/apple/pkl-neovim",
         lazy = true,
         event = "BufReadPre *.pkl",

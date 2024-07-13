9adc5c05adfe4d4cd7a77a42bbf4ef5381662280
Andreas Wachs <andreas@wachs.dk>
2024-07-13 16:54:51 +0200

    Add swift

--- init.lua
+++ init.lua
@@ -320,7 +320,7 @@ require('lspconfig').gleam.setup{}
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'yaml', 'zig', 'gleam', 'terraform', 'json', 'elixir'},
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'yaml', 'zig', 'gleam', 'terraform', 'json', 'elixir', 'swift'},
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
